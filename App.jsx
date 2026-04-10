c660b22c300a5d7c4e4ff1f51047ae9ecae54a2d
shaqibul-neil <shaqibul.islam.neil@gmail.com>
2026-04-11 03:38:29 +0600

    TouchableOpacity

--- App.jsx
+++ App.jsx
@@ -1,9 +1,16 @@
-import { Image, Text, View } from 'react-native';
+import { Button, Image, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 
 const App = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 20,
+      }}
+    >
       <Text style={{ fontSize: 60, fontWeight: 'bold' }}>Hello World</Text>
       <Image
         source={{
@@ -11,6 +18,15 @@ const App = () => {
         }}
         style={{ width: 200, height: 200 }}
       />
+      {/* <Button title="Click Me" onPress={() => alert('Button Pressed!')} /> */}
+      <TouchableOpacity
+        style={{ backgroundColor: 'blue', padding: 10, borderRadius: 5 }}
+        onPress={() => alert('Button Pressed!')}
+      >
+        <Text style={{ color: 'white', fontWeight: 'bold' }}>
+          Click Me button
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
