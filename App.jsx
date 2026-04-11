181fac729828c8b00df5cb8f635ba42ec124ca0d
shaqibul-neil <shaqibul.islam.neil@gmail.com>
2026-04-11 16:37:26 +0600

    using flatlist to show multiple item with map

--- App.jsx
+++ App.jsx
@@ -1,57 +1,101 @@
-import {
-  Image,
-  Pressable,
-  Text,
-  TouchableOpacity,
-  useColorScheme,
-} from 'react-native';
+import { FlatList, Image, Text, View } from 'react-native';
 import React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
+
 import styles from './styles/App.styles';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const App = () => {
   //Mobile current theme
-  const theme = useColorScheme();
-  const isDarkMode = theme === 'dark';
-  const backgroundColor = isDarkMode ? '#000' : '#d1faff';
-  const TextColor = isDarkMode ? 'white' : '#012c31';
+  // const theme = useColorScheme();
+  // const isDarkMode = theme === 'dark';
+  // const backgroundColor = isDarkMode ? '#000' : '#d1faff';
+  // const TextColor = isDarkMode ? 'white' : '#012c31';
+
+  const data = [
+    {
+      id: 1,
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      image: 'https://randomuser.me/api/portraits/men/1.jpg',
+    },
+    {
+      id: 2,
+      name: 'Michael Smith',
+      email: 'michael.smith@example.com',
+      image: 'https://randomuser.me/api/portraits/men/2.jpg',
+    },
+    {
+      id: 3,
+      name: 'David Johnson',
+      email: 'david.johnson@example.com',
+      image: 'https://randomuser.me/api/portraits/men/3.jpg',
+    },
+    {
+      id: 4,
+      name: 'Chris Brown',
+      email: 'chris.brown@example.com',
+      image: 'https://randomuser.me/api/portraits/men/4.jpg',
+    },
+    {
+      id: 5,
+      name: 'James Wilson',
+      email: 'james.wilson@example.com',
+      image: 'https://randomuser.me/api/portraits/men/5.jpg',
+    },
+    {
+      id: 6,
+      name: 'Emily Davis',
+      email: 'emily.davis@example.com',
+      image: 'https://randomuser.me/api/portraits/women/1.jpg',
+    },
+    {
+      id: 7,
+      name: 'Sophia Martinez',
+      email: 'sophia.martinez@example.com',
+      image: 'https://randomuser.me/api/portraits/women/2.jpg',
+    },
+    {
+      id: 8,
+      name: 'Olivia Taylor',
+      email: 'olivia.taylor@example.com',
+      image: 'https://randomuser.me/api/portraits/women/3.jpg',
+    },
+    {
+      id: 9,
+      name: 'Isabella Anderson',
+      email: 'isabella.anderson@example.com',
+      image: 'https://randomuser.me/api/portraits/women/4.jpg',
+    },
+    {
+      id: 10,
+      name: 'Ava Thomas',
+      email: 'ava.thomas@example.com',
+      image: 'https://randomuser.me/api/portraits/women/5.jpg',
+    },
+  ];
 
   return (
-    <SafeAreaView
-      style={[styles.container, { backgroundColor: backgroundColor }]}
-      edges={['top', 'bottom', 'left', 'right']}
-    >
-      <Text style={[styles.text1, { color: TextColor }]}>Hello World</Text>
-      <Text style={[styles.text2, { color: TextColor }]}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-        incidunt nobis rerum sunt aperiam cupiditate aliquid quidem architecto
-        minima repellat.
-      </Text>
-      <Image
-        style={styles.image}
-        source={{
-          uri: 'https://reactnative.dev/img/tiny_logo.png',
-        }}
+    <SafeAreaView style={styles.container}>
+      {/* Always use flatlist to display lists of data -- shows only the items that are currently visible on the screen -- memory efficient -- donot use scrollview because it loads all items at once */}
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <View key={item.id} style={styles.card}>
+            <Image source={{ uri: item.image }} style={styles.image} />
+            <View style={{ gap: 5, alignItems: 'center', textAlign: 'center' }}>
+              <Text style={styles.text1}>Name: {item.name}</Text>
+              <Text style={styles.text2}>{item.email}</Text>
+            </View>
+          </View>
+        )}
+        keyExtractor={item => item.id}
+        ItemSeparatorComponent={<View style={{ height: 16 }} />}
+        numColumns={2}
+        columnWrapperStyle={{ gap: 16, justifyContent: 'center' }}
+        // extra data refers to any additional data that the flatlist needs to re-render when it changes -- if the data is not changing, we can ignore it -- if the data is changing, we need to pass it as extra data to the flatlist so that it can re-render when the data changes
+        extraData={data}
+        // horizontal
       />
-      {/* <Button title="Click Me" onPress={() => alert('Button Pressed!')} /> */}
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => alert('Button Pressed!')}
-      >
-        <Text style={[styles.buttonText, { color: TextColor }]}>
-          Click Me button
-        </Text>
-      </TouchableOpacity>
-      {/* <TouchableHighlight onPress={() => alert('Touchable Highlight Pressed!')}>
-        <Text>Touchable Highlight</Text>
-      </TouchableHighlight> */}
-      <Pressable
-        style={styles.button}
-        onPress={() => alert('Pressable Pressed!')}
-        onHoverIn={() => console.log('Mouse entered')}
-      >
-        <Text style={styles.buttonText}>Pressables</Text>
-      </Pressable>
     </SafeAreaView>
   );
 };
--- styles/App.styles.js
+++ styles/App.styles.js
@@ -4,38 +4,49 @@ const styles = StyleSheet.create({
   container: {
     width: '100%',
     height: '100%',
+    backgroundColor: '#d1faff',
     gap: 20,
-    padding: 16,
-    flex: 1,
+    padding: 20,
+  },
+  card: {
+    width: 200,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    gap: 5,
     alignItems: 'center',
     justifyContent: 'center',
   },
   image: {
-    width: 200,
-    height: 200,
+    width: 100,
+    height: 100,
+    borderRadius: 50,
   },
 
   text1: {
-    fontSize: 40,
+    fontSize: 20,
     fontWeight: 'bold',
+    textAlign: 'center',
   },
   text2: {
-    fontSize: 20,
+    fontSize: 14,
+    textAlign: 'center',
   },
-  button: {
-    backgroundColor: '#3a3a3a',
-    borderRadius: 50,
-    borderWidth: 2,
-    borderBottomColor: '#ff0000d0',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonText: {
-    color: 'white',
-    fontWeight: 'bold',
-  },
+  //   button: {
+  //     backgroundColor: '#3a3a3a',
+  //     borderRadius: 50,
+  //     borderWidth: 2,
+  //     borderBottomColor: '#ff0000d0',
+  //     paddingVertical: 10,
+  //     paddingHorizontal: 20,
+  //     alignItems: 'center',
+  //     justifyContent: 'center',
+  //   },
+  //   buttonText: {
+  //     color: 'white',
+  //     fontWeight: 'bold',
+  //   },
 });
 
 export default styles;
